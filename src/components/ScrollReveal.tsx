19a43bf5bdfec2ce36f8053f85aea16a6c1e19f4
JustHiro55 <hagi.hiro9829@gmail.com>
2026-02-04 00:15:11 +0900

    Fix type import for ReactNode

--- src/components/ScrollReveal.tsx
+++ src/components/ScrollReveal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import type { ReactNode } from 'react'
 import { useScrollReveal } from '../hooks/useScrollReveal'
 import styles from './ScrollReveal.module.css'
 
